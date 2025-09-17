549ac7c1a67bca688f0a066204fa119cee599d18
Jeevan Reddy Y <jeevanreddy05462@gmail.com>
2025-09-17 16:41:02 +0530

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+  },
+);
